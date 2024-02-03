afb7e6a103e577fd6db3649ff994f3fcf189c37c
src1026 <sc5144@barnard.edu>
2024-02-03 10:21:28 -0500

    Initial commitment

--- pet_dating_app/views.py
+++ pet_dating_app/views.py
@@ -2,19 +2,17 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
-from .models import (PetProfile, Match, Message, Event, EventAttendee,
-                     ESAOpportunity, ESARegistration, PetBehaviorLog, MentalHealthResource)
+from .models import (PetProfile, Match, Message,
+                     ESAOpportunity, ESARegistration, PetBehaviorLog)
 from .serializers import (UserSerializer, PetProfileSerializer, MatchSerializer,
                           MessageSerializer, EventSerializer, EventAttendeeSerializer,
                           ESAOpportunitySerializer, ESARegistrationSerializer,
                           PetBehaviorLogSerializer, MentalHealthResourceSerializer)
 
-# 使用Django REST Framework的ViewSets来简化视图的创建
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]  # 例如，这里我们要求用户必须认证
+    permission_classes = [permissions.IsAuthenticated]
 
 class PetProfileViewSet(viewsets.ModelViewSet):
     queryset = PetProfile.objects.all()
@@ -31,16 +29,6 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class EventAttendeeViewSet(viewsets.ModelViewSet):
-    queryset = EventAttendee.objects.all()
-    serializer_class = EventAttendeeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
 class ESAOpportunityViewSet(viewsets.ModelViewSet):
     queryset = ESAOpportunity.objects.all()
     serializer_class = ESAOpportunitySerializer
@@ -54,9 +42,4 @@ class ESARegistrationViewSet(viewsets.ModelViewSet):
 class PetBehaviorLogViewSet(viewsets.ModelViewSet):
     queryset = PetBehaviorLog.objects.all()
     serializer_class = PetBehaviorLogSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class MentalHealthResourceViewSet(viewsets.ModelViewSet):
-    queryset = MentalHealthResource.objects.all()
-    serializer_class = MentalHealthResourceSerializer
     permission_classes = [permissions.IsAuthenticated]
